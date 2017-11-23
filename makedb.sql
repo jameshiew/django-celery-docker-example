77e4d4b7a11aa63276e88af08079042ab1f46363
James Hiew <james@hiew.net>
2017-11-23 19:34:50 +0000

    Add attribution to makedb.sql

--- makedb.sql
+++ makedb.sql
@@ -1,3 +1,4 @@
+-- adapted from https://www.digitalocean.com/community/tutorials/how-to-use-postgresql-with-your-django-application-on-ubuntu-14-04
 CREATE DATABASE webapp_db;
 CREATE USER webapp with password 'webapp';
 ALTER ROLE webapp SET client_encoding TO 'utf8';
